dd6f0e13a00f0f5eef12068a1e0e2099f99c63d9
Guto Xavier <gutoffline@gmail.com>
2026-03-12 16:24:49 -0300

    pastas

--- pastas.js
+++ pastas.js
@@ -1,9 +1,38 @@
 const fs = require('fs')
+let pasta = 'fotos'
 
-fs.access("fotos" , fs.constants.R_OK | fs.constants.W_OK, (err) => {
-  console.log('\n> Checking Permission for reading and writing to file');
-  if (err)
-    console.error('No Read and Write access');
-  else
-    console.log('File can be read and written');
-});
+console.log("\n\n===== verificando pasta =====")
+if (fs.existsSync(pasta)) {
+  console.log('Tudo certo')
+} else {
+  console.log('A pasta não existe')
+}
+
+console.log("\n\n===== criando pasta =====")
+pasta = 'icones'
+if (!fs.existsSync(pasta)) {
+  fs.mkdirSync(pasta)
+  console.log("pasta criada")
+} else {
+  console.log("pasta já existe")
+}
+
+console.log("\n\n===== listando conteúdo da pasta =====")
+pasta = 'icones'
+console.log(fs.readdirSync(pasta))
+
+console.log("\n\n===== renomeando pasta =====")
+try {
+  fs.renameSync('icones', 'icons')
+  console.log("pasta renomeado")
+} catch (erro) {
+  console.log("não foi renomeado")
+}
+
+console.log("\n\n===== excluindo pasta =====")
+const fsextra = require('fs-extra')
+
+const folder = 'icons'
+fsextra.remove(folder, erro => {
+  console.error(erro)
+})
